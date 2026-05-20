415604aa443957f8efa3f6c6804068d14edb060e
Teyfik Rumelli <teyfik@teyfik.local>
2026-05-21 00:39:10 +0200

    update favicon

--- vite.config.js
+++ vite.config.js
@@ -9,27 +9,27 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'favicon-32x32.png', 'favicon-16x16.png'],
       manifest: {
-        name: 'Deutsch Lernen PWA',
-        short_name: 'Deutsch',
+        name: 'Kartei Karten PWA',
+        short_name: 'KarteiKarten',
         description: 'Learn German via Flashcards with Spaced Repetition',
         theme_color: '#ffffff',
         background_color: '#ffffff',
         display: 'standalone',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: 'android-chrome-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
